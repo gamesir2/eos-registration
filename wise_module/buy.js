5c941e49b06d0cff6ea6a8ef6a4814e524a5f786
paullee <241805907@qq.com>
2018-09-29 16:28:03 +0800

    1

--- wise_module/buy.js
+++ wise_module/buy.js
@@ -2,13 +2,25 @@ const eos = require('./eos.js')
 const userAccount = require('./config').user
 const desposeAccout = require('./config').despose;
 
-function buyToken(eosAmount) {
-    options = {
-        authorization: userAccount,
-        broadcast: true,
-        sign: true
-    }
-    eos.transfer(userAccount,desposeAccout,eosAmount,'',options);
+async function buyToken(amount,memo) {
+    eos.transaction({
+        actions: [
+            {
+                account: 'eosio.token',
+                name: 'transfer',
+                authorization: [{
+                    actor:userAccount,
+                    permission: 'active'
+                }],
+                data: {
+                    from: userAccount,
+                    to: desposeAccout,
+                    quantity: amount,
+                    memo: memo
+                }
+            }
+        ]
+    });
 }
 
 module.exports = buyToken
